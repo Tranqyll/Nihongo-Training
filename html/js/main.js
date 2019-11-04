91b1c40b04e8be1663b87371fb96c06886fa570a
Fabien Caylus <tranqyll.dev@gmail.com>
2019-11-04 17:00:53 +0900

    Fix bugs

--- html/js/main.js
+++ html/js/main.js
@@ -2,16 +2,18 @@
 /*global Papa */
 
 BASE_DATA_URL = "data/"
-    
+
 /*
  * ----- Support section ------
  */
 
 // From http://stackoverflow.com/a/8813472/3570066
 if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(obj, start) {
+    Array.prototype.indexOf = function (obj, start) {
         for (var i = (start || 0), j = this.length; i < j; i++) {
-            if (this[i] === obj) { return i; }
+            if (this[i] === obj) {
+                return i;
+            }
         }
         return -1;
     };
@@ -23,7 +25,7 @@ if (!String.prototype.trim) {
         return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
     };
 }
-    
+
 /*
  * ----- Cookie Stuff -----
  */
@@ -40,9 +42,9 @@ function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
     var i;
-    for(i = 0; i < ca.length; i++) {
+    for (i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') {
+        while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
         if (c.indexOf(name) === 0) {
@@ -53,42 +55,42 @@ function getCookie(cname) {
 }
 
 function checkCookiesOnStart() {
-    if(navigator.cookieEnabled === true) {
+    if (navigator.cookieEnabled === true) {
         var random = getCookie("randomButton");
-        if(random == 'true') {
-            document.getElementById('randomButton').checked = true;    
+        if (random == 'true') {
+            document.getElementById('randomButton').checked = true;
         }
-        
+
         var limitBut = getCookie("limitButton");
-        if(limitBut == 'true') {
+        if (limitBut == 'true') {
             document.getElementById('limitButton').checked = true;
             document.getElementById('numberOfWords').disabled = false;
         }
-        
+
         var limit = getCookie("numberOfWords");
-        if(limit !== "") {
-            document.getElementById('numberOfWords').value = parseInt(limit);    
+        if (limit !== "") {
+            document.getElementById('numberOfWords').value = parseInt(limit);
         }
-        
+
         var rd_jptoen = getCookie("radio_jptoen");
-        if(rd_jptoen == 'true') {
-            document.getElementById('radio_jptoen').checked = true;    
+        if (rd_jptoen == 'true') {
+            document.getElementById('radio_jptoen').checked = true;
         }
         var rd_entojp = getCookie("radio_entojp");
-        if(rd_entojp == 'true') {
-            document.getElementById('radio_entojp').checked = true;    
+        if (rd_entojp == 'true') {
+            document.getElementById('radio_entojp').checked = true;
         }
         var rd_both = getCookie("radio_both");
-        if(rd_both == 'true') {
-            document.getElementById('radio_both').checked = true;    
+        if (rd_both == 'true') {
+            document.getElementById('radio_both').checked = true;
         }
     }
 }
-    
+
 /*
  * ----- General functions -----
  */
-    
+
 // From http://stackoverflow.com/a/6274398/3570066
 function shuffle(array) {
     var counter = array.length;
@@ -110,81 +112,82 @@ function shuffle(array) {
 }
 
 // From http://stackoverflow.com/a/1026087/3570066
-String.prototype.capitalizeFirstLetter = function() {
+String.prototype.capitalizeFirstLetter = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
-    
-String.prototype.replaceAll = function(search, replacement) {
+
+String.prototype.replaceAll = function (search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
 // percent must be an integer in range [0,100]
 function moveProgressBarTo(percent) {
-    var elem = document.getElementById("resultsProgress");   
+    var elem = document.getElementById("resultsProgress");
     var width = 0;
     elem.style.width = "0%";
     elem.className = "w3-progressBar w3-red";
     var id = setInterval(frame, 10);
+
     function frame() {
         if (width >= percent) {
-             clearInterval(id);
+            clearInterval(id);
         } else {
-            width++; 
-            elem.style.width = width + '%'; 
-            document.getElementById("resultsProgressLabel").innerHTML = width * 1  + '%';
-              
+            width++;
+            elem.style.width = width + '%';
+            document.getElementById("resultsProgressLabel").innerHTML = width * 1 + '%';
+
             // Update color
-            if(width == 40) {
+            if (width == 40) {
                 elem.className = "w3-progressBar w3-deep-orange";
-            } else if(width == 65) {
+            } else if (width == 65) {
                 elem.className = "w3-progressBar w3-orange";
-            } else if(width == 85) {
+            } else if (width == 85) {
                 elem.className = "w3-progressBar w3-green";
             }
         }
     }
 }
-    
+
 // Retrieve a list from a csv file using papa parse
 function getCSVList(path, onFinishedFunction) {
     Papa.parse(BASE_DATA_URL + path, {
         download: true,
         delimiter: "=",
         fastMode: true,
-        complete: function(results, file) {
+        complete: function (results, file) {
             // Papa parse appends an empty item at the end of the list
             // So, remove it
-            while(results.data[results.data.length-1][0] === "") {
+            while (results.data[results.data.length - 1][0] === "") {
                 results.data.pop();
             }
-            
+
             onFinishedFunction(results.data);
         }
     });
 }
-    
+
 // Return a concatanation of all subjects for a book
 function getAllSubjects(bookId, onFinishedFunction) {
-    getCSVList(bookId + ".csv", function(data) {
+    getCSVList(bookId + ".csv", function (data) {
         var mainList = [];
         var subCount = data.length;
         var i = 0;
-        var handleSubRequest = function(subData) {
+        var handleSubRequest = function (subData) {
             mainList = mainList.concat(subData);
-            i = i+1;
-            if(i >= subCount) {
+            i = i + 1;
+            if (i >= subCount) {
                 // All subjects have been retrieved
                 onFinishedFunction(mainList);
             } else {
                 getCSVList("subjects/" + data[i][0] + ".csv", handleSubRequest);
             }
         };
-        
+
         getCSVList("subjects/" + data[i][0] + ".csv", handleSubRequest);
     });
 }
-    
+
 /*
  * ----- Highlight the selected item -----
  */
@@ -196,12 +199,12 @@ function updateList(className, id) {
         el[i].className = className;
     }
     document.getElementById(id).className = className + " w3-theme";
-}    
-    
+}
+
 /*
  * ----- Books / subjects  -----
  */
-    
+
 // Return a list with: [id, text]
 function selectedItem(className) {
     var id = "";
@@ -217,33 +220,35 @@ function selectedItem(className) {
     }
     return [id, text];
 }
-    
+
 function showBookOptions() {
     var book = selectedItem("book");
-    
+
+    document.getElementById("modalSubjectTitle").innerHTML = book[1];
+
     // Now parse the corresponding map file
-    getCSVList(book[0] + ".csv", function(data) {
+    getCSVList(book[0] + ".csv", function (data) {
         var subList = document.getElementById("modalSubjectList");
         // Remove previous entries
-        while (subList.hasChildNodes()) {   
+        while (subList.hasChildNodes()) {
             subList.removeChild(subList.firstChild);
         }
-            
+
         var i;
-        for(i = 0; i < data.length; i++) {
+        for (i = 0; i < data.length; i++) {
             var sub = data[i];
-            if(i === 0) {
+            if (i === 0) {
                 subList.innerHTML = subList.innerHTML + '<li id="' + sub[0] + '" onclick="updateList(' + "'sub','" + sub[0] + "'" + ')" class="w3-theme sub">' + sub[1] + '</li>';
             } else {
                 subList.innerHTML = subList.innerHTML + '<li id="' + sub[0] + '" onclick="updateList(' + "'sub','" + sub[0] + "'" + ')" class="sub" >' + sub[1] + '</li>';
             }
         }
-        
-        // Add a "all" element
+
+        // Add "all" element
         subList.innerHTML = subList.innerHTML + '<li id="sub_all" onclick="updateList(' + "'sub','sub_all'" + ')"  class="sub">All subjects !</li>';
-        
+
         // Show the modal
-        document.getElementById('modalChooseSubject').style.display='block';
+        document.getElementById('modalChooseSubject').style.display = 'block';
     });
 }
 
@@ -262,73 +267,73 @@ var currentId;
 
 function startTraining() {
     // Called when the word list have been retrieved
-    var handleWordsList = function(data) {
+    var handleWordsList = function (data) {
         currentWordsArray = data;
-            
+
         successMap = [];
         userInputsHistory = [];
         currentId = 0;
-            
+
         // Check if the array must be shuffled
-        if(document.getElementById('randomButton').checked) {
+        if (document.getElementById('randomButton').checked) {
             currentWordsArray = shuffle(currentWordsArray);
         }
-            
+
         // Check if the user select a limit
-        if(document.getElementById('limitButton').checked) {
+        if (document.getElementById('limitButton').checked) {
             var wordsLimit = document.getElementById('numberOfWords').value;
-            if(wordsLimit < currentWordsArray.length) {
+            if (wordsLimit < currentWordsArray.length) {
                 currentWordsArray = currentWordsArray.slice(0, wordsLimit);
             }
         }
-            
+
         var i;
         // Check the translation direction
-        if(document.getElementById('radio_jptoen').checked) {
+        if (document.getElementById('radio_jptoen').checked) {
             // User answers in english
-            for(i = 0; i < currentWordsArray.length; i++) {
+            for (i = 0; i < currentWordsArray.length; i++) {
                 translateDirection.push(0);
             }
-        } else if(document.getElementById('radio_entojp').checked) {
+        } else if (document.getElementById('radio_entojp').checked) {
             // User answers in japanase
-            for(i = 0; i < currentWordsArray.length; i++) {
+            for (i = 0; i < currentWordsArray.length; i++) {
                 translateDirection.push(1);
             }
-        } else if(document.getElementById('radio_both').checked) {
+        } else if (document.getElementById('radio_both').checked) {
             // Users answers both in japanese or english
-            for(i = 0; i < currentWordsArray.length; i++) {
+            for (i = 0; i < currentWordsArray.length; i++) {
                 translateDirection.push(Math.round(Math.random()));
             }
         }
-            
-        if(translateDirection[0] === 0) {
+
+        if (translateDirection[0] === 0) {
             document.getElementById("trainingTranslateLabel").innerHTML = "Translation in <b>English</b>:";
         } else {
             document.getElementById("trainingTranslateLabel").innerHTML = "Translation in <b>Japanese</b>:";
         }
-            
+
         document.getElementById("trainingWord").innerHTML = currentWordsArray[0][translateDirection[0]].capitalizeFirstLetter().replaceAll(";", ",");
         document.getElementById("trainingButtonNext").innerHTML = "Next (" + (currentId + 1) + "/" + currentWordsArray.length + ")";
         document.getElementById('trainingUserInput').value = "";
-            
-        document.getElementById('onStartDiv').style.display='none';
-        document.getElementById('resultsDiv').style.display='none';
-            
+
+        document.getElementById('onStartDiv').style.display = 'none';
+        document.getElementById('restartDiv').style.display = 'none';
+        document.getElementById('resultsDiv').style.display = 'none';
+
         // Show the training part
-        document.getElementById('trainingDiv').style.display='block';
-            
-        // Hide the modal
-        document.getElementById('modalChooseSubject').style.display='none';
-        document.getElementById('modalChooseTheme').style.display='none';
-        document.getElementById('modalChooseBook').style.display='none';
+        document.getElementById('trainingDiv').style.display = 'block';
+
+        // Hide the modals
+        document.getElementById('modalChooseBook').style.display = 'none';
+        document.getElementById('modalChooseSubject').style.display = 'none';
     };
-    
+
     var book = selectedItem("book");
     var sub = selectedItem("sub");
-    
+
     document.getElementById("trainingTitle").innerHTML = book[1] + ": " + sub[1];
-    
-    if(sub[0] == "sub_all") {
+
+    if (sub[0] == "sub_all") {
         getAllSubjects(book[0], handleWordsList);
     } else {
         getCSVList("subjects/" + sub[0] + ".csv", handleWordsList);
@@ -337,26 +342,26 @@ function startTraining() {
 
 function removeUselessWords(str) {
     var s = str.replaceAll("-", " ")
-    .replaceAll('"', " ")
-    .replaceAll("‘", " ")
-    .replaceAll("’", " ")
-    .replaceAll("»", " ")
-    .replaceAll("«", " ")
-    .replaceAll(",", " ")
-    .replaceAll("/", " ")
-    .replaceAll("”", " ")
-    .replaceAll("“", " ")
-    .replaceAll("\\?", "")
-    .replaceAll("!", "")
-    .replaceAll(";", " ")
-    .replaceAll(":", " ")
-    .replaceAll("'", " ")
-    .replaceAll("\\.", "")
-    .replaceAll(" %", "%");
-    
+        .replaceAll('"', " ")
+        .replaceAll("‘", " ")
+        .replaceAll("’", " ")
+        .replaceAll("»", " ")
+        .replaceAll("«", " ")
+        .replaceAll(",", " ")
+        .replaceAll("/", " ")
+        .replaceAll("”", " ")
+        .replaceAll("“", " ")
+        .replaceAll("\\?", "")
+        .replaceAll("!", "")
+        .replaceAll(";", " ")
+        .replaceAll(":", " ")
+        .replaceAll("'", " ")
+        .replaceAll("\\.", "")
+        .replaceAll(" %", "%");
+
     return s.trim();
 }
-    
+
 function removeParenthesis(str, inside) {
     if (inside === true) {
         // Remove everything inside parenthesis
@@ -370,54 +375,54 @@ function processNextWord() {
     // First step: check current word
     var input = document.getElementById('trainingUserInput').value;
     userInputsHistory.push(input);
-    
+
     input = input.trim();
     // Remove double spaces
-    input = input.replace(/ +(?= )/g,'');
+    input = input.replace(/ +(?= )/g, '');
     // Remove accents
     input = input.latinise();
     input = input.toLowerCase();
     input = removeUselessWords(input);
-    
+
     // Split translations in several ones 
-    var translations = currentWordsArray[currentId][1-translateDirection[currentId]].split(";");
+    var translations = currentWordsArray[currentId][1 - translateDirection[currentId]].split(";");
     var match = false;
     var i;
-    for(i = 0; i < translations.length; i++) {
+    for (i = 0; i < translations.length; i++) {
         var trans = translations[i];
         trans = trans.trim();
-        trans = trans.replace(/ +(?= )/g,'');
+        trans = trans.replace(/ +(?= )/g, '');
         trans = trans.latinise();
         trans = trans.toLowerCase();
         trans = removeUselessWords(trans);
-        
+
         // Check if the translation contains parenthesis
         if (trans.indexOf("(") > -1) {
             var trans2 = removeParenthesis(trans, true);
-            if(trans2 == input || trans2.score(input) >= 0.8) {
+            if (trans2 == input || trans2.score(input) >= 0.8) {
                 match = true;
                 break;
             }
             trans = removeParenthesis(trans, false);
         }
-        
+
         // Standard check (with words inside parenthesis)
-        if(trans == input || trans.score(input) >= 0.8) {
+        if (trans == input || trans.score(input) >= 0.8) {
             match = true;
             break;
         }
     }
-    
+
     successMap.push(match);
-    
+
     // Second step: move to the next word
     currentId += 1;
-    if(currentId < currentWordsArray.length) {
+    if (currentId < currentWordsArray.length) {
         // Update information on screen
         //document.getElementById('trainingWord').classList.toggle('Sw3-animate-right');
         //document.getElementById('trainingWord').classList.toggle('w3-animate-right');
-        
-        if(translateDirection[currentId] === 0) {
+
+        if (translateDirection[currentId] === 0) {
             document.getElementById("trainingTranslateLabel").innerHTML = "Translation in <b>English</b>:";
         } else {
             document.getElementById("trainingTranslateLabel").innerHTML = "Translation in <b>Japanese</b>:";
@@ -427,36 +432,38 @@ function processNextWord() {
         document.getElementById('trainingUserInput').value = "";
     } else {
         // The user reach the end of the words list
-        document.getElementById('trainingDiv').style.display='none'; // Hide training div
-        document.getElementById('resultsDiv').style.display='block'; // Show results div
-        
+        document.getElementById('trainingDiv').style.display = 'none'; // Hide training div
+        document.getElementById('resultsDiv').style.display = 'block'; // Show results div
+
+        document.getElementById('restartDiv').style.display = 'block';
+
         // Update progress bar
         var percent = 0;
-        for(i = 0; i < successMap.length; i++) {
-            if(successMap[i]) {
+        for (i = 0; i < successMap.length; i++) {
+            if (successMap[i]) {
                 percent += 1;
             }
         }
-        percent = parseInt((percent/successMap.length)*100);
+        percent = parseInt((percent / successMap.length) * 100);
         moveProgressBarTo(percent);
-        
+
         //Update entries list
         var results = document.getElementById("resultsHistoryTable");
-        while (results.hasChildNodes()) {   
+        while (results.hasChildNodes()) {
             results.removeChild(results.firstChild);
         }
-        
+
         results.innerHTML = results.innerHTML + '<tr><th>Japanese</th><th>English</th><th>Your answer</th></tr>';
-        
-        for(i = 0; i < successMap.length; i++) {
-            if(successMap[i]) {
+
+        for (i = 0; i < successMap.length; i++) {
+            if (successMap[i]) {
                 results.innerHTML = results.innerHTML + '<tr class="w3-green"><td>' + currentWordsArray[i][0].capitalizeFirstLetter().replaceAll(";", ",") + '</td><td>' + currentWordsArray[i][1].capitalizeFirstLetter().replaceAll(";", ",") + '</td><td>' + userInputsHistory[i] + '</td></tr>';
             } else {
                 results.innerHTML = results.innerHTML + '<tr class="w3-red"><td>' + currentWordsArray[i][0].capitalizeFirstLetter().replaceAll(";", ",") + '</td><td>' + currentWordsArray[i][1].capitalizeFirstLetter().replaceAll(";", ",") + '</td><td>' + userInputsHistory[i] + '</td></tr>';
             }
         }
     }
-    
+
     // Avoid refresh
     return false;
 }
